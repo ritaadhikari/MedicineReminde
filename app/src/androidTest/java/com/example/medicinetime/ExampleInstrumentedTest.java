80e6072d22004a226c235036fecf1bffb2011d88
Rita adhikari <ritaadhikari936@gmail.com>
2023-03-02 14:40:27 +0545

    Ui changed

--- app/src/androidTest/java/com/example/medicinetime/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/medicinetime/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getContext();
 
-        assertEquals("com.gautam.medicinetime", appContext.getPackageName());
+        assertEquals("com.example.medicinetime", appContext.getPackageName());
     }
 }
